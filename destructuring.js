87ae044aa474e84b79b756de639c36ac502e311a
Mark Todman <93592366+marktodman@users.noreply.github.com>
2022-06-10 17:20:46 +0200

    Initial commit

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node destructuring.js in the terminal
+ */
+
+// Destructuring arrays
+
+// Destructuring objects
+
+// Destructuring subsets
+
+// Using rest parameter syntax
